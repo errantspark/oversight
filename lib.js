0df670a36b739698b0ecb5c12eba3684996bb2b1
Matt Vana <errantspark@gmail.com>
2016-12-10 06:05:56 -0800

    shit lookin tite now

--- lib.js
+++ lib.js
@@ -1,11 +1,7 @@
 let rescale = (min, max) => s => (s-min)/(max-min)
 let getMeta = projects => {
   let meta = {}
-  let dates = projects.map(x => {
-    if (x.hasGit && x.gitLog.date) {
-      return x.gitLog.date
-    }
-  }).filter(x => x)
+  let dates = projects.map(x => x.date)
   meta.newest = Math.max(...dates)
   meta.oldest = Math.min(...dates)
   return meta
